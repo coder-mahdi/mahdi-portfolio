cf8fa8a0babd9205b60459548b47a2b9a77c0b57
mahdi-mim <mahdiroozbahani25@gmail.com>
2024-10-03 15:03:47 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header'; 
 import Sidebar from './components/sidebar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Sidebar/>
+      <Footer/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,9 @@ function Header() {
 
   return (
     <header>
-      <h1>{headerData.title}</h1>
+       {headerData.logo && (
+        <img src={process.env.PUBLIC_URL + '/' + headerData.logo} alt="Logo" style={{ width: "50px", height: "50px" }} />
+      )}
       <nav>
         {headerData.navLinks.map((link, index) => (
           <a key={index} href={link.link}>
